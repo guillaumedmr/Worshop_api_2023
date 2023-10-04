86a5a932024b7dd95c9a01e483eb5e1a9373055e
GuillaumeDMR <guillaume@daumur.fr>
2023-10-04 11:40:43 +0200

    getAllMission

--- controllers/missionController.js
+++ controllers/missionController.js
@@ -2,46 +2,37 @@ const connection = require('../services/connectBDD');
 const axios = require('axios');
 const key_api_map = process.env.SECRET_API_MAPBOX;
 
-    async function fetchMapboxData(string) {            
-        try {
-            // Définissez l'URL de l'API Mapbox
-            const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${string}.json?access_token=${key_api_map}`;
-        
-            // Effectuez la requête GET à l'API Mapbox
-            const response = await axios.get(apiUrl);
-        
-            // Vérifiez si la requête a réussi
-            if (response.status === 200) {
-              // Récupérez les données de réponse JSON
-              const data = response.data;
+module.exports.getAllMission = async (req, res) => {
+  const sql = `
+  SELECT m.m_nom, m.m_adresse, m.m_location, m.m_date, m.m_nombre_participant, m.m_description, m.m_majeur, c.c_name as categorie, o.o_nom as organisateur, o.o_telephone as telephone_organisateur, o.o_mail as mail_organisateur
+  FROM mission m
+  JOIN categorie c ON c.c_id = m.m_id_categorie 
+  JOIN organisateur o ON o.o_id = m.m_id_organisateur
+  WHERE m.m_date_suppression IS NULL;
+  `
 
-              return data.features[0].center;
-            } else {
-              throw new Error('La requête vers l\'API Mapbox a échoué.');
-            }
-          } catch (error) {
-            console.error('Erreur lors de la requête Mapbox :', error);
-            throw error;
-          }
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de la requête SQL :', err);
+      res.status(500).json({ message: 'Une erreur est survenue lors de la récupération des missions.' });
+    } else {
+      // Renvoyer les résultats en format JSON
+      res.status(200).json(results);
     }
-
-    module.exports.map = async (req, res) => {
-        const search = req.body['search'];
-        console.log(fetchMapboxData(search));
-    }
-
+  });
+}
 
 module.exports.create = async (req, res) => {
     if (req.session.loggedInUser) {
         try {
             // Récupérez les données du formulaire d'inscription depuis req.body
-            const { m_nom, m_adresse, m_date, m_nombre_participant, m_description, m_id_categorie } = req.body;  
+            const { m_nom, m_adresse, m_date, m_nombre_participant, m_description, m_id_categorie, m_majeur } = req.body;  
         
             // Insérez les données dans la base de données
             const sql = `
                 INSERT INTO mission
-                (m_nom, m_adresse, m_location, m_date, m_nombre_participant, m_id_organisateur, m_description, m_id_categorie)
-                VALUES(?, ?, ?, ?, ?, ?, ?, ?);
+                (m_nom, m_adresse, m_location, m_date, m_nombre_participant, m_id_organisateur, m_description, m_id_categorie, m_majeur)
+                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);
             `;
 
             const apiUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${m_adresse}.json?access_token=${key_api_map}`;
@@ -60,7 +51,7 @@ module.exports.create = async (req, res) => {
         
                 connection.query(
                 sql,
-                [m_nom, m_adresse, m_location, m_date, m_nombre_participant, m_id_organisateur, m_description, m_id_categorie],
+                [m_nom, m_adresse, m_location, m_date, m_nombre_participant, m_id_organisateur, m_description, m_id_categorie, m_majeur],
                 (err, results) => {
                     if (err) {
                     console.error('Erreur lors de la création :', err);
--- routes/missionRoutes.js
+++ routes/missionRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const missionController = require("../controllers/missionController.js")
 
+router.get("/getAllMission", missionController.getAllMission);
 router.post("/create", missionController.create);
-router.post("/map", missionController.map);
 
 module.exports = router
